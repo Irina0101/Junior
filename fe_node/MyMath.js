e65f06634dafc1893592bf46b140b3afee5ac2ca
Ivan <ivan.kodak.02@gmail.com>
2022-01-20 14:25:53 +0200

    Initial project

---
+++ fe_node/MyMath.js
@@ -0,0 +1,7 @@
+class MyMath{
+  static sum = (...args)=>args.reduce((a,i)=>a+i, 0);
+  static sub = (a,b) => a - b;
+  static mul = (a,b) => a * b;
+}
+
+exports.MyMath = MyMath;
--- fe_node/index.js
+++ fe_node/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const {MyMath} = require('./MyMath');
+/*const {MyMath} = require('./MyMath');
 
 console.log(MyMath);
 
-console.log('Hello node');
+console.log('Hello node');*/
+
+const fs = require('fs').promises;
+fs.readFile('./text.txt', 'utf8').then((content) => {
+  const newContentInNewFile = 'Old Content:${content} \nNew Content: LOREM IPSUM';
+  fs.writeFile('./newFile.txt', newContentInNewFile, 'utf8');
+});
